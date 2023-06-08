81ede313f48db9042cd681b7d7f7923e0b6ac013
techniekteam-passie-utrecht <techniekteampassieutrecht@gmail.com>
2023-06-08 13:29:29 +0200

    added some midi related functions

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from termcolor import colored
+# import subprocess
 import pygame.midi
 import pygame
+import os
 import time
 
 
 def debounce(lst):
-    result = []
-    for i in range(len(lst)):
-        if i == 0 or lst[i] != lst[i-1]:
-            result.append(lst[i])
-    return result
+	result = []
+	for i in range(len(lst)):
+		if i == 0 or lst[i] != lst[i-1]:
+			result.append(lst[i])
+	return result
 
 class XTouch:
 
 	def __init__ (self, ip_port, op_port):
 		self.input  : pygame.midi.Input  = pygame.midi.Input (ip_port)
 		self.output : pygame.midi.Output = pygame.midi.Output(op_port)
+
+	def close(self):
+		self.input.close()
+		self.output.close()
 
 	def send_midi(self, type, data):
 		types = {
@@ -29,7 +35,7 @@ class XTouch:
 
 
 	def send_sysex(self, data):
-		...
+		raise(NotImplementedError)
 	
 	def get_data(self) -> list:
 		types = {
@@ -46,11 +52,31 @@ class XTouch:
 			data.append([t, *m[1:]])
 		return debounce(data)
 			
-	def led_on(self, number):
-		if number > 0 and number <= 93:
-			self.send_midi('note_on', [number+8, 127])
-		else:
-			self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
+	def reset_controls(self):
+		for i in range(119):
+			self.send_midi('note_on', [i, 0])
+			self.send_midi('control_change', [i, 0])
+
+	def led_on(self, *numbers):
+		for number in numbers:
+			if number >= 0 and number <= 93:
+				self.send_midi('note_on', [number+8, 127])
+			else:
+				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
+	
+	def led_off(self, *numbers):
+		for number in numbers:
+			if number >= 0 and number <= 93:
+				self.send_midi('note_on', [number+8, 0])
+			else:
+				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
+		
+	def led_all_off(self):
+		for i in range(94):
+			self.led_off(i)
+
+
+	
 
 	def error(self, message):
 		print(colored(f'ERROR: {message}', 'white', 'on_red'))
@@ -62,6 +88,10 @@ class MyDmx:
 	def __init__(self, ip_port, op_port):
 		self.input  : pygame.midi.Input  = pygame.midi.Input (ip_port)
 		self.output : pygame.midi.Output = pygame.midi.Output(op_port)
+
+	def close(self):
+		self.input.close()
+		self.output.close()
 		
 
 	def send_midi(self, type, data):
@@ -97,27 +127,46 @@ class MyDmx:
 		print(colored(f'WARNING: {message}', 'white', 'on_blue'))
 
 
-pygame.init()
-pygame.midi.init()
-
-xt = XTouch(1,6)
-md = MyDmx(3,8)
-
-# for i in range(127):
-#     xt.send_midi('note_on', [i, 127])
-#     xt.send_midi('control_change', [i, 127])
-#     time.sleep(.1)
 
 
-# print(debounce([3, 3, 3, 4, 6, 6, 7]))
 
-while True:
-	d = xt.get_data()
-	if d: print(d)
-	for m in d:
-		md.send_midi(m[0], m[1:])
-	
-	e = md.get_data()
-	if e: print(e)
-	for m in e:
-		xt.send_midi(m[0], m[1:])
+
+if __name__ == '__main__':
+	try:
+		pygame.init()
+		pygame.midi.init()
+
+		xt = XTouch(1,6)
+		md = MyDmx(3,8)
+		# uncomment below if you want to launch mydmx
+		# os.startfile(r'C:\Users\Licht computer\Desktop\licht-files\aula_v_8.0.dvc')
+		
+		# for i in range(94):
+		# 	xt.led_on(i)
+		# 	time.sleep(.1)
+
+		xt.reset_controls()
+		
+		while True:
+			d = xt.get_data()
+			#if d: print(d)
+			for m in d:
+				md.send_midi(m[0], m[1:])
+			
+			e = md.get_data()
+			if e: print(e)
+			for m in e:
+				xt.send_midi(m[0], m[1:])
+	# except:
+	# 	pass
+	finally:
+		try:
+			xt.close()
+			md.close()
+		except:
+			pass
+		pygame.midi.quit()
+		pygame.quit()
+		# uncomment below if you want the mydmx closing warnings
+		# os.system(r'msg * Please close and save changes in the MyDMX software')
+		# print(colored('Please close and save changes in the MyDMX software.', 'white', 'on_red'))
