eb84735ab8fe38db257637cda6c230ea0a5a5944
tommie1236 <timooosterom@gmail.com>
2023-06-12 20:40:29 +0200

    cleaned up code 1/x

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from termcolor import colored
-# import subprocess
-from segments import SEGMENTS # 7-segment data
-import pygame.midi
-import pygame
 import argparse
-import keyboard
 import os
 import time
+
+import keyboard
+import pygame
+import pygame.midi
+from termcolor import colored
+
+from segments import SEGMENTS  # 7-segment data
 
 KEYS = {96: 'up',
 		97: 'down',
@@ -21,8 +22,7 @@ COLORS = {
 		'blue'	 : 0b100,
 		'magenta': 0b101,
 		'cyan'	 : 0b110,
-		'white'	 : 0b111
-}
+		'white'	 : 0b111}
 
 
 
@@ -55,32 +55,48 @@ def setup_midi():
 				print('Invalid index input. Please enter a valid index (integer).')
 		return tuple(ports)
 		
-
-	
-
 def setup_argparser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-d', "--default-ports", action='store_true', help='use the default midi ports (xtouch 1, 6 mydmx 4, 8)') 
 	return parser.parse_args()
 
+
+
 class XTouch:
 	def __init__ (self, ip_port, op_port):
-		self.input  : pygame.midi.Input  = pygame.midi.Input (ip_port)
-		self.output : pygame.midi.Output = pygame.midi.Output(op_port)
-		self.segments = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-		self.dots = [0b0000000, 0b00000]
-		self.strips1 = [[0x00 for _ in range(7)] for _ in range(8)]
-		self.strips2 = [[0x00 for _ in range(7)] for _ in range(8)]
-		self.backlight = [0b000000 for _ in range(8)]
-		self.channelbank = 0
-		self.presetsbank = 0
-		self.mode = 'channel'
+		"""Initiate a connection with the 'behringer x-touch one' MIDI controller"""
+		self.ip_port = ip_port
+		self.op_port = op_port
+		self.input  : pygame.midi.Input  = pygame.midi.Input (self.ip_port)	# connect to MIDI in port of x-touch
+		self.output : pygame.midi.Output = pygame.midi.Output(self.op_port)	# connect to MIDI out port of x-touch
+		self.segments = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]	# set segment display data to all off
+		self.dots = [0b0000000, 0b00000]								# set segment dots to all off
+		self.strips1 = [[0x00 for _ in range(7)] for _ in range(8)]		# create list of 8 displays of 7 chars (top display)
+		self.strips2 = [[0x00 for _ in range(7)] for _ in range(8)]		# create list of 8 displays of 7 chars (bottom display)
+		self.backlight = [0b000000 for _ in range(8)]					# create list of 8x 0b00000 for backlight
+		self.channelbank = 0 											# set the channel-bank to 0
+		self.presetsbank = 0 											# set the presets-bank to 0
+		self.mode = 'channel'											# set mode to channel
+		self.leds = [0 for _ in range(93)]								# create a list of 93 zeroes
 
 	def close(self):
+		"""close the MIDI ports"""
 		self.input.close()
 		self.output.close()
 
+	def connect(self):
+		"""Connect to the x-touch midi controller.
+		Not needed unless you closed the connection.
+		"""
+		self.input  = pygame.midi.Input (self.ip_port)
+		self.output = pygame.midi.Output(self.op_port)
+
 	def _send_midi(self, type: str, data: list):
+		"""Sends midi message to x-touch controller
+		Args:
+		- type (str)	Type of the MIDI message ('note_on', 'note_off', 'control_change')
+		- data (list)	A list of bytes to send to the MIDI controller.
+		"""
 		types = {
 			'note_on': 0x90,
 			'note_off': 0x80,
@@ -94,59 +110,62 @@ class XTouch:
 				self.error(f'midi data <{[type, *data]}> not send')
 
 	def _send_sysex(self, data: bytes):
-			self.output.write_sys_ex(pygame.midi.time(), data)
+		"""Send a SysEx message to the MIDI controller.
+		Used in for example updating the 7-segment displays and LCD-Scribble-strips.
+		Args:
+		- data (bytes)	The data you want to send to the MIDI controller
+		"""
+		self.output.write_sys_ex(pygame.midi.time(), data)
 	
 	def update_segment_display(self):
-		# print(self.segments)
+		"""Update the 7-segment displays to the data in self.segments
+		Use the XTouch.set_segment_data() function to set the segment data
+		"""
 		self._send_sysex([0xf0, 0x00, 0x20, 0x32, 0x14, 0x37, *self.segments, *self.dots, 0xf7])
 
 	def clear_segments_display(self):
-		self.segments = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+		"""Clear (and update) the 7-segment displays.
+		"""
+		self.segments = [0x00 for _ in range(12)]		# create list of 12 times 0x00
 		self.update_segment_display()
 
 	def set_segment_data(self, idx: int, chars: str):
+		'''Sets the segment data to something
+		Args: 
+		- idx 	(int) The index of the segment you want to (start) update. (0-11)
+		- chars (str) The characher(s) you want to put at the specified index.
 		'''
-		sets the segment data to something
-		args: 
-		- idx, the index of the segment you want to updated. (0-11)
-		- chars, the charachers you want to put 
-		'''
-		if not isinstance(idx, int):
-			self.warning(f'index needs to be a integer.')
-		if idx < 0 or idx > 11:
-			self.error(f'segment index <{idx}> out of range, needs to be 0-11.')
-			return False
-		if idx + len(chars) > 12:
-			self.warning(f"chars: <{chars}> doesn't fit in the display, characthers are cut off.")
+		if not isinstance(idx, int):	self.warning(f'index needs to be a integer.')												# check if idx is a integer
+		if idx < 0 or idx > 11:			self.error(f'segment index <{idx}> out of range, needs to be 0-11.'); return 				# check if idx is in range
+		if idx + len(chars) > 12:		self.warning(f"chars: <{chars}> doesn't fit in the display, characthers are cut off.")		# check if chars fit on screen
+		
 		for char in chars:
-			if idx > 11:
-				break
+			if idx > 11:	break
 			char = char.lower()
+
 			if char in SEGMENTS.keys():
 				self.segments[idx] = SEGMENTS[char]
 				# first clear the dot at a specified index
-				if idx < 7:
-					self.dots[0] &= 0b0 << idx
-				else:
-					self.dots[1] &= 0b0 << (idx - 7)
-			elif char == '.': 	# then activate the dot if it needs to
-				if idx < 7:
-					self.dots[0] |= 0b1 << idx
-				else:
-					self.dots[1] |= 0b1 << (idx - 7)
+				if idx < 7:	self.dots[0] &= 0b0 << idx
+				else:		self.dots[1] &= 0b0 << (idx - 7)
+
+			elif char == '.': 	# then activate the dot if it needs to be activated
+				if idx < 7:	self.dots[0] |= 0b1 << idx
+				else:		self.dots[1] |= 0b1 << (idx - 7)
 				continue	# skip incrementing the idx var to not skip the next segment because the dot is in the same segment as the last char
-			else:
-				self.warning(f'char <{char}> not a valid segment characther')
+
+			else:	self.warning(f'char <{char}> not a valid segment characther')
 			idx += 1
+
 		self.update_segment_display()
 
-	def update_scribble_strip(self, display: int):
+	def update_scribble_strip(self, display: int):			# TODO docstring and comments
 		if display < 0 or display > 7:
 			self.error(f'Display <{display}> not valid. (0-7)\nData not send!')
 			return
 		self._send_sysex([0xf0, 0x00, 0x20, 0x32, 0x14, 0x4c, display, self.backlight[display], *self.strips1[display], *self.strips2[display], 0xf7])
 
-	def set_scribble_strip_color(self, display: int, color: str, inv_top: bool = False, inv_bottom: bool = False):
+	def set_scribble_strip_color(self, display: int, color: str, inv_top: bool = False, inv_bottom: bool = False):	# TODO docstring and comments
 		""" backlight
 		0bxxxyz
 		xxx - color
@@ -167,16 +186,16 @@ class XTouch:
 		self.backlight[display] |= inv_top << 4
 		self.backlight[display] |= inv_bottom << 5
 
-	def clear_scribble_strip(self, display: int):
+	def clear_scribble_strip(self, display: int):	# TODO docstring and comments
 		self.strips1[display] = [0b00 for _ in range(7)]
 		self.strips2[display] = [0b00 for _ in range(7)]
 		self.update_scribble_strip(display)
 
-	def clear_scribble_strips(self):
+	def clear_scribble_strips(self):	# TODO docstring and comments
 		for i in range(8):
 			self.clear_scribble_strip(i)
 
-	def set_scribble_strip_data(self, display: int, topidx: int, topchars: str, bottomidx: int, bottomchars: str):
+	def set_scribble_strip_data(self, display: int, topidx: int, topchars: str, bottomidx: int, bottomchars: str):	# TODO docstring and comments
 		# set top display data
 		if not isinstance(topidx, int): self.warning(f'Topidx needs to be a integer.')
 		if topidx < 0 or topidx > 7: self.error(f'Character index <{topidx}> out of range. (0-7)')
@@ -198,7 +217,7 @@ class XTouch:
 
 		self.update_scribble_strip(display)
 
-	def update_bank(self ,change: int):
+	def update_bank(self ,change: int):	# TODO docstring and comments
 		bank = getattr(self, f'{self.mode}bank')
 		bank += change
 		if bank < 0: bank = 99
@@ -213,7 +232,7 @@ class XTouch:
 		# self.set_segment_data(0, f'{self.banknr:02d}')
 		# print(colored(f'Bank set to: {self.banknr:02d}', 'green'))
 
-	def set_bank_nr(self, bank: str, number: int):
+	def set_bank_nr(self, bank: str, number: int):	# TODO docstring and comments
 		if bank == 'channels':
 			self.channelbank = number
 			self.set_segment_data(0, f'{self.channelbank:02d}')
@@ -221,7 +240,7 @@ class XTouch:
 			self.presetsbank = number
 			self.set_segment_data(0, f'{self.presetsbank:02d}')
 
-	def update_mode(self, mode = None):
+	def update_mode(self, mode = None):	# TODO docstring and comments
 		if mode: self.mode = mode
 		if self.mode == 'channel':
 			self.led_on(86)
@@ -233,60 +252,67 @@ class XTouch:
 			self.set_segment_data(0, f'{self.presetsbank:02d}')
 		self.set_segment_data(2, f'{self.mode:7}')
 
-	def get_data(self) -> list:
+	def get_data(self) -> list:	# TODO docstring and comments
 		types = {
 			0x90: 'note_on',
 			0x80: 'note_off',
 			0xb0: 'control_change'}
 
 		data_in = self.input.read(10)
-		data= []
-		for d in data_in:
-			m = d[0]
-			t = types[m[0]]
+		data = [[types[d[0][0]], *d[0][1:]] for d in data_in]
 
-			data.append([t, *m[1:]])
+		# compressed into list comprehention but if it gets problems the working code is commented below
+
+		# for d in data_in:
+		# 	m = d[0]
+		# 	t = types[m[0]]
+		# 	data.append([t, *m[1:]])
+
 		return debounce(data)
 			
-	def reset_controls(self):
+	def reset_controls(self):	# TODO docstring and comments
 		for i in range(119):
 			self._send_midi('note_on', [i, 0])
 			self._send_midi('control_change', [i, 0])
 
-	def led_on(self, *numbers):
+	def led_on(self, *numbers):	# TODO docstring and comments
 		for number in numbers:
 			if number >= 0 and number <= 93:
 				self._send_midi('note_on', [number+8, 127])
 			else:
 				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
 	
-	def led_off(self, *numbers):
+	def led_off(self, *numbers):	# TODO docstring and comments
 		for number in numbers:
 			if number >= 0 and number <= 93:
 				self._send_midi('note_on', [number+8, 0])
 			else:
 				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
 		
-	def led_all_off(self):
+	def led_toggle(self, *numbers):	# TODO docstring and comments
+		for number in numbers:
+			...
+
+	def led_all_off(self):	# TODO docstring and comments
 		for i in range(94):
 			self.led_off(i)
 
-	def led_all_on(self):
+	def led_all_on(self):	# TODO docstring and comments
 		for i in range(94):
 			self.led_on(i)
 	
-	def all_faders_up(self):
+	def all_faders_up(self):	# TODO docstring and comments
 		for i in range(20):
 			self._send_midi('control_change', [i + 70, 127])
-			time.sleep(.1)
+			time.sleep(.05)
 
-	def error(self, message: str):
+	def error(self, message: str):	# TODO docstring and comments
 		print(colored(f'ERROR: {message}', 'white', 'on_red'))
 
-	def warning(self, message: str):
+	def warning(self, message: str):	# TODO docstring and comments
 		print(colored(f'WARNING: {message}', 'white', 'on_blue'))
 
-class MyDmx:
+class MyDmx:	# TODO docstring and comments
 	def __init__(self, ip_port, op_port):
 		self.input  : pygame.midi.Input  = pygame.midi.Input (ip_port)
 		self.output : pygame.midi.Output = pygame.midi.Output(op_port)
@@ -312,12 +338,14 @@ class MyDmx:
 			0xb0: 'control_change'}
 		
 		data_in = self.input.read(10)
-		data= []
-		for d in data_in:
-			m = d[0]
-			t = types[m[0]]
+		data = [[types[d[0][0]], *d[0][1:]] for d in data_in]
 
-			data.append([t, *m[1:]])
+		# compressed into list comprehention but if it gets problems the working code is commented below
+
+		# for d in data_in:
+		# 	m = d[0]
+		# 	t = types[m[0]]
+		# 	data.append([t, *m[1:]])
 		
 		return debounce(data)
 
@@ -327,7 +355,7 @@ class MyDmx:
 	def warning(self, message):
 		print(colored(f'WARNING: {message}', 'white', 'on_blue'))
 
-class BPM:
+class BPM:	# TODO docstring and comments
 	def __init__(self):
 		self.bpm = 0
 		self.maxdata = 2
@@ -370,11 +398,11 @@ if __name__ == '__main__':
 
 		# startup sequence
 		xt.led_all_on()
-		# xt.all_faders_up()
-		# xt.set_segment_data(0, '0123456789ab')
-		# time.sleep(.5)
+		xt.all_faders_up()
+		xt.set_segment_data(0, '0123456789ab')
+		time.sleep(.5)
 		xt.reset_controls()
-		# xt.clear_segments_display()
+		xt.clear_segments_display()
 
 		xt.set_bank_nr('channel', 0)
 		xt.set_bank_nr('presets', 0)
@@ -383,10 +411,9 @@ if __name__ == '__main__':
 
 		xt.update_segment_display()
 		
-		xt.set_scribble_strip_color(0, 'red')
-		xt.set_scribble_strip_data(0, 0, 'Display', 0, '1')
-		xt.set_scribble_strip_color(0, 'green')
-		xt.set_scribble_strip_data(1, 0, 'Display', 0, '2')
+		for i in range(7):
+			xt.set_scribble_strip_color(i, 'white')
+			xt.set_scribble_strip_data(0, 0, 'Display', 0, f'{i}')
 
 
 
@@ -419,9 +446,7 @@ if __name__ == '__main__':
 									bpm.bpm_pulse()
 									bpm.calculate_bpm()
 								elif m[2] == 0:	xt.led_off(93)
-								continue
-							
-															
+								continue							
 
 						if m[1] in [96, 97, 98, 99]:
 							if m[2] == 127:
@@ -430,8 +455,7 @@ if __name__ == '__main__':
 							elif m[2] == 0:
 								xt.led_off(m[1] - 8)
 								keyboard.release(KEYS[m[1]])
-							continue
-							
+							continue	
 
 					if m[0] == 'control_change':
 						match m[1]:
@@ -458,16 +482,11 @@ if __name__ == '__main__':
 							ENCODERS = [max(0, min(value, 127)) for value in ENCODERS] # cap the values in the list between 0 and 127
 							continue
 
-
-
-					# md._send_midi(m[0], m[1:]) # uncomment to send all midi data to mydmx3
-
 			e = md.get_data()
 			if e:
 				print(e)
 				for m in e:
 					...
-					# xt._send_midi(m[0], m[1:]) # uncomment to send all midi data to x-touch
 
 	finally:
 		try:
