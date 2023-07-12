08cec35860d382aa26860f8613031bde5d552104
tommie1236 <timooosterom@gmail.com>
2023-07-12 22:01:48 +0200

    did some random optimizations, started the banks

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
 import json
 import os
+import platform
 import time
+from dataclasses import dataclass
 
-from dotwiz import DotWiz
 import keyboard
 import pygame
 import pygame.midi
@@ -17,6 +18,7 @@ KEYS = {96: 'up',
 		99: 'right'}
 ENCODERS = [0, 0, 0 ,0 ,0 ,0 ,0 ,0]
 COLORS = {
+		'off'	 : 0b000,
 		'black'	 : 0b000,
 		'red'	 : 0b001,	
 		'green'	 : 0b010,
@@ -25,10 +27,12 @@ COLORS = {
 		'magenta': 0b101,
 		'cyan'	 : 0b110,
 		'white'	 : 0b111}
+PRESETS_BANKS = []
+CHANNEL_BANKS = []
 
 
 
-def debounce(lst):
+def debounce(lst) -> list:
 	result = []
 	for i in range(len(lst)):
 		if i == 0 or lst[i] != lst[i-1]:
@@ -49,13 +53,13 @@ def setup_midi() -> tuple:
 			try:
 				i = int(input(f'Enter index of <{port[0]}>: '))
 				if 0 <= i < dev_count:
-					ports[port] = i
+					ports.append(i)
 					break
 				else:
 					print('Invalid MIDI device index. Try again.')
 			except ValueError:
 				print('Invalid index input. Please enter a valid index (integer).')
-		return tuple(ports)
+	return tuple(ports)
 		
 def setup_argparser() -> argparse.Namespace:
 	parser = argparse.ArgumentParser()
@@ -63,9 +67,16 @@ def setup_argparser() -> argparse.Namespace:
 	return parser.parse_args()
 
 
+def load_presets(bank):
+	...
+
+def load_banks(mode):
+	...
+
+
 
 class XTouch:
-	def __init__ (self, ip_port, op_port):
+	def __init__ (self, ip_port, op_port) -> None:
 		"""Initiate a connection with the 'behringer x-touch one' MIDI controller"""
 		self.ip_port = ip_port
 		self.op_port = op_port
@@ -81,19 +92,19 @@ class XTouch:
 		self.mode = 'channel'												# set mode to channel
 		self.leds = [0 for _ in range(94)]									# create a list of 94 zeroes
 
-	def close(self):
+	def close(self) -> None:
 		"""close the MIDI ports"""
 		self.input.close()
 		self.output.close()
 
-	def connect(self):
+	def connect(self) -> None:
 		"""Connect to the x-touch midi controller.
 		Not needed unless you closed the connection.
 		"""
 		self.input  = pygame.midi.Input (self.ip_port)
 		self.output = pygame.midi.Output(self.op_port)
 
-	def _send_midi(self, type: str, data: list):
+	def _send_midi(self, type: str, data: list) -> None:
 		"""Sends midi message to x-touch controller
 		Args:
 		- type (str)	Type of the MIDI message ('note_on', 'note_off', 'control_change')
@@ -111,7 +122,7 @@ class XTouch:
 			except:
 				self.error(f'midi data <{[type, *data]}> not send')
 
-	def _send_sysex(self, data: bytes):
+	def _send_sysex(self, data: bytes) -> None:
 		"""Send a SysEx message to the MIDI controller.
 		Used in for example updating the 7-segment displays and LCD-Scribble-strips.
 		Args:
@@ -119,19 +130,19 @@ class XTouch:
 		"""
 		self.output.write_sys_ex(pygame.midi.time(), data)
 	
-	def update_segment_display(self):
+	def update_segment_display(self) -> None:
 		"""Update the 7-segment displays to the data in self.segments
 		Use the XTouch.set_segment_data() function to set the segment data
 		"""
 		self._send_sysex([0xf0, 0x00, 0x20, 0x32, 0x14, 0x37, *self.segments, *self.dots, 0xf7])
 
-	def clear_segments_display(self):
+	def clear_segments_display(self) -> None:
 		"""Clear (and update) the 7-segment displays.
 		"""
 		self.segments = [0x00 for _ in range(12)]		# create list of 12 times 0x00
 		self.update_segment_display()
 
-	def set_segment_data(self, idx: int, chars: str):
+	def set_segment_data(self, idx: int, chars: str) -> None:
 		'''Sets the segment data to something
 		Args: 
 		- idx 	(int) The index of the segment you want to (start) update. (0-11)
@@ -161,7 +172,7 @@ class XTouch:
 
 		self.update_segment_display()
 
-	def update_scribble_strip(self, display: int):			# TODO docstring and comments
+	def update_scribble_strip(self, display: int) -> None:			# TODO docstring and comments
 		if display < 0 or display > 7:
 			self.error(f'Display <{display}> not valid. (0-7)\nData not send!')
 			return
@@ -188,21 +199,21 @@ class XTouch:
 		self.backlight[display] |= inv_top << 4
 		self.backlight[display] |= inv_bottom << 5
 
-	def clear_scribble_strip(self, display: int):	# TODO docstring and comments
+	def clear_scribble_strip(self, display: int) -> None:	# TODO docstring and comments
 		self.strips1[display] = [0b00 for _ in range(7)]
 		self.strips2[display] = [0b00 for _ in range(7)]
 		self.update_scribble_strip(display)
 
-	def clear_scribble_strips(self):	# TODO docstring and comments
+	def clear_scribble_strips(self) -> None:	# TODO docstring and comments
 		for i in range(8):
 			self.clear_scribble_strip(i)
 
-	def reset_scribble_strips(self):
+	def reset_scribble_strips(self) -> None:
 		for i in range(8):
 			self.set_scribble_strip_color(i, 'white')
 		self.clear_scribble_strips()
 
-	def set_scribble_strip_data(self, display: int, topchars: str, bottomchars: str):	# TODO docstring and comments
+	def set_scribble_strip_data(self, display: int, topchars: str, bottomchars: str) -> None:	# TODO docstring and comments
 		# set top display data
 		topidx = 0
 		if not isinstance(topidx, int): self.warning(f'Topidx needs to be a integer.')
@@ -226,24 +237,19 @@ class XTouch:
 
 		self.update_scribble_strip(display)
 
-	def update_bank(self ,change: int):	# TODO docstring and comments
+	def update_bank(self ,change: int) -> None:	# TODO docstring and comments
 		bank = getattr(self, f'{self.mode}bank')
 		bank += change
-		if bank < 0: bank = 99
-		if bank > 99: bank = 0
+		if self.mode == 'preset':
+			if bank < 0: bank = 99
+			if bank > 99: bank = 0
+		elif self.mode == 'channel':
+			if bank < 0: bank = 64
+			if bank > 64: bank = 0
 		setattr(self, f'{self.mode}bank', bank)
 		self.set_segment_data(0, f'{bank:02d}')
-		bank_ = Bank(getattr(self, f'{self.mode}bank'))
-		bank_.load()
-		
-		# old one bank system below, maybe handy later to look back on. isn't needed in production anymore
-		# self.banknr += change
-		# if self.banknr < 0: self.banknr = 99
-		# if self.banknr > 99: self.banknr = 0
-		# self.set_segment_data(0, f'{self.banknr:02d}')
-		# print(colored(f'Bank set to: {self.banknr:02d}', 'green'))
 
-	def set_bank_nr(self, bank: str, number: int):	# TODO docstring and comments
+	def set_bank_nr(self, bank: str, number: int) -> None:	# TODO docstring and comments
 		if bank == 'channels':
 			self.channelbank = number
 			self.set_segment_data(0, f'{self.channelbank:02d}')
@@ -251,7 +257,8 @@ class XTouch:
 			self.presetsbank = number
 			self.set_segment_data(0, f'{self.presetsbank:02d}')
 
-	def update_mode(self, mode = None):	# TODO docstring and comments
+	def update_mode(self, mode = None) -> None:	# TODO docstring and comments
+		org_mode = self.mode
 		if mode: self.mode = mode
 		if self.mode == 'channel':
 			self.led_on(86)
@@ -261,6 +268,9 @@ class XTouch:
 			self.led_on(87)
 			self.led_off(86)
 			self.set_segment_data(0, f'{self.presetsbank:02d}')
+		else:
+			self.mode = org_mode
+			self.error(f'Mode <{mode}> Invalid, Stayting on <{self.mode}>')
 		self.set_segment_data(2, f'{self.mode:7}')
 
 	def get_data(self) -> list:	# TODO docstring and comments
@@ -281,12 +291,12 @@ class XTouch:
 
 		return debounce(data)
 			
-	def reset_controls(self):	# TODO docstring and comments
+	def reset_controls(self) -> None:	# TODO docstring and comments
 		for i in range(119):
 			self._send_midi('note_on', [i, 0])
 			self._send_midi('control_change', [i, 0])
 
-	def led_on(self, *numbers):	# TODO docstring and comments
+	def led_on(self, *numbers) -> None:	# TODO docstring and comments
 		for number in numbers:
 			if number >= 0 and number <= 93:
 				self._send_midi('note_on', [number+8, 127])
@@ -294,7 +304,7 @@ class XTouch:
 			else:
 				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
 	
-	def led_off(self, *numbers):	# TODO docstring and comments
+	def led_off(self, *numbers) -> None:	# TODO docstring and comments
 		for number in numbers:
 			if number >= 0 and number <= 93:
 				self._send_midi('note_on', [number+8, 0])
@@ -302,7 +312,7 @@ class XTouch:
 			else:
 				self.warning(f'Button <{number}> is not valid, must be greater than 0 and cant be greater than 93')
 		
-	def led_toggle(self, *numbers):	
+	def led_toggle(self, *numbers) -> None:	
 		"""Toggles the specified LED(S)
 		Args:
 		- number (int | list) The LED(S) you want to toggle on/off. (0-93)
@@ -311,23 +321,23 @@ class XTouch:
 			if   self.leds[number] == 0:	self.led_on (number);	self.leds[number] = 1 	# Toggle on
 			elif self.leds[number] == 1:	self.led_off(number);	self.leds[number] = 0 	# Toggle off
 
-	def led_all_off(self):	# TODO docstring and comments
+	def led_all_off(self) -> None:	# TODO docstring and comments
 		for i in range(94):
 			self.led_off(i)
 
-	def led_all_on(self):	# TODO docstring and comments
+	def led_all_on(self) -> None:	# TODO docstring and comments
 		for i in range(94):
 			self.led_on(i)
 	
-	def all_faders_up(self):	# TODO docstring and comments
+	def all_faders_up(self) -> None:	# TODO docstring and comments
 		for i in range(20):
 			self._send_midi('control_change', [i + 70, 127])
 			time.sleep(.05)
 
-	def error(self, message: str):	# TODO docstring and comments
+	def error(self, message: str) -> None:	# TODO docstring and comments
 		print(colored(f'ERROR: {message}', 'white', 'on_red'))
 
-	def warning(self, message: str):	# TODO docstring and comments
+	def warning(self, message: str) -> None:	# TODO docstring and comments
 		print(colored(f'WARNING: {message}', 'white', 'on_blue'))
 
 class MyDmx:	# TODO docstring and comments
@@ -391,53 +401,49 @@ class BPM:	# TODO docstring and comments
 			# print(f'{between:5} {len(self.data):2} {self.bpm} {self.data}')
 		except: pass
 		xt.set_segment_data(9, f'{self.bpm:03d}')
-	
-class Presets:	# TODO docstring and comments
-	def __init__(self):
-		self.data = {}
 
-	def load_file(self, file: str = 'presets.json'):
-		self.file = file
-		with open(self.file, 'r') as file:
-			self.data = json.load(file)
-
-	def save_file(self, file: str = None, indent: int = 4):
-		if file is None: file = self.file
-		with open(file, 'w') as file:
-			json.dump(self.data, file, indent)
-
-	def get_channel(self, bank: int, channel: int):
-		return self.data['presets'][f'bank_{bank}'][f'channel_{channel}']
+@dataclass	
+class Preset:
+	number: int
+	name: str
+	color: str
+	encoder_value: int
+	encoder_led_value: int
+	led_bar_value: int
+	button_1_value: bool
+	button_2_value: bool
+	button_3_value: bool
+	button_4_value: bool
+	fader_value: int
 	
-	def load_bank(self, banknr):
-		return self.data['presets'][f'bank_{banknr}']
-	
+
+
 class Bank:
-	def __init__(self, banknr) -> None:
-		self.banknr = banknr
-	
-	def load(self):
-		for i in range(8):
-			xt.set_scribble_strip_data(i, str(i), presets.load_bank(self.banknr)[f'channel_{i}']['name'])
+	def __init__(self) -> None:
+		pass
+
+
+
 
 
 if __name__ == '__main__':
 	try:
+		if platform.system() == 'Windows':
+			from colorama import just_fix_windows_console
+			just_fix_windows_console()
 		args = setup_argparser()
 		pygame.init()
 		pygame.midi.init()
 		if args.default_ports:
 			print(colored('Using default MIDI ports: (xtouch 1, 6 mydmx 4, 8).', 'white', 'on_green'))
-			xt = XTouch(1,6)
-			md = MyDmx (4,8)
+			xt = XTouch(1, 6)	# 1, 6
+			md = MyDmx (4, 8)	# 4, 8
 		else:
 			ports: tuple = setup_midi()
 			print(ports)
 			xt = XTouch(*ports[0:2])
 			md = MyDmx (*ports[2:4])
 		bpm = BPM()
-		presets = Presets()
-		presets.load_file(r'presets/presets.json')
 		bpm.bpm_pulse()
 		# uncomment below if you want to launch mydmx
 		# os.startfile(r'C:\Users\Licht computer\Desktop\licht-files\aula_v_8.0.dvc')
@@ -457,7 +463,7 @@ if __name__ == '__main__':
 		for i in range(8):
 			xt.set_scribble_strip_data(i, 'Display', f'{i}')
 		
-		time.sleep(1)
+		time.sleep(1) 	# startup animation delay
 		xt.reset_controls()
 		xt.clear_segments_display()
 		xt.reset_scribble_strips()
@@ -531,17 +537,15 @@ if __name__ == '__main__':
 								ENCODERS[m[1] - 80] -= 9
 							elif m[2] == 65:
 								ENCODERS[m[1] - 80] += 9
-							ENCODERS = [max(0, min(value, 127)) for value in ENCODERS] # cap the values in the list between 0 and 127
-							for i, encoder in enumerate(ENCODERS):
-								xt._send_midi('control_change', [i + 80, encoder, 0])
-							continue
+							ENCODERS[m[1] - 80] = max(0, min(ENCODERS[m[1] - 80], 127)) # cap the value of the encoderbetween 0 and 127
+							xt._send_midi('control_change', m[1:])	# send the same message back to update the led ring
+							continue 
 
 			e = md.get_data()
 			if e:
 				print(e)
 				for m in e:
-					...
-
+					xt._send_midi(m[0], m[1:])
 	finally:
 		try:
 			xt.clear_segments_display()
@@ -557,3 +561,4 @@ if __name__ == '__main__':
 			# uncomment below if you want the mydmx closing warnings
 			# os.system(r'msg * Please close and save changes in the MyDMX software')
 			# print(colored('Please close and save changes in the MyDMX software.', 'white', 'on_red'))
+
